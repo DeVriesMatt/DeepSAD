e2bdf37f1665e510317cb8565f8664a0c2d15b84
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-05 16:09:53 +0100

    updated run1

--- src/run_deepSAD_1.sh
+++ src/run_deepSAD_1.sh
@@ -12,7 +12,7 @@ do
 	    fi
 		for gamma_l in 0.01 0.05 0.1 0.2
 		do
-			python main.py fmnist fmnist_LeNet ../log/fmnist/scenario_1/deepSAD/ ../data --ratio_known_outlier gamma_l --ratio_pollution 0.1 \
+			python main.py fmnist fmnist_LeNet ../log/fmnist/scenario_1/deepSAD/ ../data --ratio_known_outlier $gamma_l --ratio_pollution 0.1 \
 			--lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 \
 			--ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class $normal_class --known_outlier_class $unknown_class \
 			--n_known_outlier_classes 1 --seed 0 --n_jobs_dataloader 6 --case 3
